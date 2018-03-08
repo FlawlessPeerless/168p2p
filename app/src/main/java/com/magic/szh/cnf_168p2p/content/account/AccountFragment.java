cab15365705af38aef584581a45aa34459cfec22
suzhenhui <362680581@qq.com>
2018-03-08 16:40:55 +0800

    # 个人账户布局完成

--- app/src/main/java/com/magic/szh/cnf_168p2p/content/account/AccountFragment.java
+++ app/src/main/java/com/magic/szh/cnf_168p2p/content/account/AccountFragment.java
@@ -2,10 +2,13 @@ package com.magic.szh.cnf_168p2p.content.account;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 
 import com.magic.szh.cnf_168p2p.R;
 import com.magic.szh.cnf_168p2p.base.BaseFragment;
+
+import butterknife.BindView;
 
 /**
  * project: CNF_168p2p
@@ -13,10 +16,13 @@ import com.magic.szh.cnf_168p2p.base.BaseFragment;
  * file: AccountFragment
  * author: admin
  * date: 2018/2/22
- * description:
+ * description: 用户账户
  */
 
 public class AccountFragment extends BaseFragment {
+    @BindView(R.id.refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
     @Override
     public Object setLayout() {
         return R.layout.fragment_account;
@@ -24,13 +30,32 @@ public class AccountFragment extends BaseFragment {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
+        initLayout();
     }
 
     /**
      * 加载数据
      */
     public void loadData() {
+        // todo 数据加载
+    }
+
+    /**
+     * 初始化布局
+     */
+    private void initLayout() {
+        mSwipeRefreshLayout.setOnRefreshListener(() -> {
+            if (mSwipeRefreshLayout.isRefreshing()) return;
+            initData();
+            mSwipeRefreshLayout.setRefreshing(false);
+
+        });
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initData() {
 
     }
 }
